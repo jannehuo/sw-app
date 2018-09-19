3a82dad7c55cde7cfff4a1a7058c88b9e91cd86c
Huotari Janne <huotaja@hfmac63.laatukolmio.fi>
2018-09-19 15:32:29 +0300

    fetch data from backend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Character from './Modules/Character.js'
+const urls = {
+  characters: '//localhost:3000/characters/'
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      characters:null
+    }
+  }
+  componentDidMount() {
+    fetch(urls.characters)
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      this.setState({
+        characters:data
+      })
+    })
+  }
   render() {
+    let CharList;
+    if(this.state.characters !== null) {
+      CharList = this.state.characters.results.map(char => {
+        return <Character name={char.name} key={char.name} />
+      });
+    }
     return (
       <div className="App">
-        Jee
+        {CharList}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/Modules/Character.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Character = ({name}) => {
+    return (
+        <div className="character-card">
+            {name}
+        </div>
+    )
+}
+
+export default Character
